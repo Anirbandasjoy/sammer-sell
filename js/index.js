2fa87910d056b48583b8398476fc540dfd57869b
joydas <joy600508@gmail.com>
2023-08-19 22:00:32 +0600

    added total price

--- js/index.js
+++ js/index.js
@@ -1,10 +1,24 @@
+let itemCount = 0;
+let totalAmout = 0;
 function handelClick(data) {
+  itemCount++;
   const nameList = getId("nameList");
   const li = document.createElement("li");
-  li.innerText = data.childNodes[3].childNodes[3].innerText;
-  console.log(nameList.appendChild(li));
+  li.innerText = `${itemCount}  . ${data.childNodes[3].childNodes[3].innerText}`;
+  li.classList.add("font-bold");
+  nameList.appendChild(li);
+  const price = Number(
+    data.childNodes[3].childNodes[5].innerText.split(" ")[0]
+  );
+  const totalPrice =
+    data.parentNode.parentNode.parentNode.parentNode.childNodes[3].childNodes[3]
+      .childNodes[3].childNodes[1].childNodes[1];
+  console.log(totalPrice);
+  totalAmout = totalAmout + price;
+  totalPrice.innerText = totalAmout.toFixed(2);
 }
 
+// getId
 function getId(id) {
   return document.getElementById(id);
 }
